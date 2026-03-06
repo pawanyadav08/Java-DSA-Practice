f8885eede5f52a3103d98fb094ca9dfb21e9bf65
Pawan Kumar Yadav <pawanydv010@gmail.com>
2026-03-06 18:44:33 +0530

    Added phase 1 basic Java problems

---
+++ palindromenum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class palindromenum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int orignal = n;
+        int rev = 0;
+
+        while(n!=0){
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n/10;
+        }
+        if (orignal == rev){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not Palindrome");
+        }
+    }
+}
