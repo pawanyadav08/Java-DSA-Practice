30dfee7df25299188c125ed60713f0aceb23211d
Pawan Kumar Yadav <pawanydv010@gmail.com>
2026-08-24 22:15:02 +0530

    New Commit

--- twodarrayquestion.java
+++ twodarrayquestion.java
@@ -1,2 +1,25 @@
 public class twodarrayquestion {
+    public static void main(String[] args) {
+        int arr[][] = {{1,2,3},{21,20,19}};
+        int maxValue = arr[0][0];
+
+        for(int i = 0 ; i<arr.length ; i++){
+            for(int j = 0 ; j<arr[i].length ; j++){
+                if(maxValue < arr[i][j]){
+                    maxValue = arr[i][j];
+                }
+            }
+        }
+        System.out.println("The maximum value is " + maxValue);
+//        int mul = 1;
+//
+//        for(int i = 0 ; i < arr.length; i++){
+//            for(int j = 0 ; j < arr[i].length ; j++){
+////                sum = sum + arr[i][j];
+//                mul = mul * arr[i][j];
+//            }
+//        }
+//        System.out.println("The mul is = " + mul);
+
+    }
 }
