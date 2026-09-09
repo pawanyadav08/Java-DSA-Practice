eeee426079ef517a5c8e8970b1e431b5c0f24fed
Pawan Kumar Yadav <pawanydv010@gmail.com>
2026-09-09 13:54:51 +0530

    Some methods of Map

---
+++ HashMapBasics.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapBasics {
+    public static void main(String[] args) {
+        Map<String, String> mapping = new HashMap<>();
+        mapping.put("in","India");
+//        mapping.put("in","India2");
+        mapping.put("en","Emgland");
+        mapping.put("us","United States");
+
+        System.out.println(mapping);
+
+        Map<String, String> table = new HashMap<>();
+        table.put("br" , "brazil");
+        System.out.println("Before: " + table);
+        table.putAll(mapping);
+        System.out.println("After: " + table);
+
+        table.remove("en");
+        System.out.println(table);
+        table.putIfAbsent("is","India3");
+        System.out.println(table);
+        System.out.println(table.get("br"));
+        System.out.println(table.getOrDefault("usa","NONE"));
+        System.out.println(table.containsKey("im"));
+        System.out.println(table.containsValue("United States of America"));
+//        System.out.println(table.size());
+    }
+}
