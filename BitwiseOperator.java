83518847ec3eeb4cc91552418cc50d398bf48b01
Pawan Kumar Yadav <pawanydv010@gmail.com>
2026-09-09 21:29:18 +0530

    Bitwise Operator Completed

---
+++ BitwiseOperator.java
@@ -0,0 +1,47 @@
+public class BitwiseOperator {
+    public static void main(String[] args) {
+//        int a = 5;
+//        int b = 6;
+//        System.out.println(a & b);
+//        System.out.println(a | b);
+//        System.out.println(a ^ b);
+//        System.out.println(~a);
+//        int n = 1;
+//        for (int i = 0 ; i <= 34 ; i++){
+//            n = n << 1;
+//            System.out.println(n);
+//            System.out.println();
+//        }
+//        int n = 100;
+//        for (int i = 1 ; i <= 10 ; i++){
+//            n = n >> 1;
+//            System.out.println(n);
+//            System.out.println();
+//        }
+//        int n = 10;
+//        if((n & 1) == 0){
+//            System.out.println("Even");
+//        }else{
+//            System.out.println("Odd");
+//        }
+//
+//        int n = 7;
+//        int count = 0;
+//        while(n!= 0){
+//            if((n&1) != 0){
+//                count++;
+//            }
+//            n = n >>1;
+//        }
+//        System.out.println("Set bit count: " + count);
+//        int n = 12;
+//        if((n & (n-1)) ==0){
+//            System.out.println("Power of 2 hai");
+//        }else {
+//            System.out.println("Power of 2 nahi hai");
+//        }
+//        System.out.println(3 << 2);
+        int n = 10;
+        System.out.println((n&(-n)));
+    }
+}
