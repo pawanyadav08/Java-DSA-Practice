7057892e575051ddbb75c159764033aceddc5eb1
Pawan Kumar Yadav <pawanydv010@gmail.com>
2026-09-11 21:58:16 +0530

    Reverse an array

--- ArrayQuestionPart2.java
+++ ArrayQuestionPart2.java
@@ -1,2 +1,25 @@
 public class ArrayQuestionPart2 {
+
+    static void reverseArray(int arr[]){
+        int n = arr.length;
+        int i = 0;
+        int j = n -1;
+
+        while (i <= j ){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        for(int k: arr){
+            System.out.println(k);
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        reverseArray(arr);
+        //2 pointer
+
+    }
 }
