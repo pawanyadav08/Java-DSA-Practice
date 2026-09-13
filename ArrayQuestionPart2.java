37b7724dc1e3da162dd24515164bf0556513b4e2
Pawan Kumar Yadav <pawanydv010@gmail.com>
2026-09-13 20:59:42 +0530

    New Questions

--- ArrayQuestionPart2.java
+++ ArrayQuestionPart2.java
@@ -1,6 +1,9 @@
 public class ArrayQuestionPart2 {
 
     static void reverseArray(int arr[]){
+        //2 pointer
+        //tc = O(n)
+        //sc = O(1)
         int n = arr.length;
         int i = 0;
         int j = n -1;
@@ -16,10 +19,42 @@ public class ArrayQuestionPart2 {
             System.out.println(k);
         }
     }
+
+    static void shiftBy1(int[] arr){
+        int n = arr.length;
+        int temp = arr[n-1];
+        for (int i = n-1 ; i > 0; i--){
+            arr[i] = arr[i-1];
+        }
+        arr[0] = temp;
+    }
+
+    static void printAlternate(int arr[]){
+        int n = arr.length;
+        int i = 0;
+        int j = n -1;
+        while(i <= j) {
+            if (i == j) {
+                System.out.println(arr[i]);
+                return;
+            }
+            else {
+                System.out.println(arr[i]);
+                i++;
+                System.out.println(arr[j]);
+                j--;
+            }
+        }
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5};
-        reverseArray(arr);
-        //2 pointer
+        printAlternate(arr);
+//        reverseArray(arr);
+//        shiftBy1(arr);
+//        for(int a: arr){
+//            System.out.print(a + " ");
+//        }
+
 
     }
 }
