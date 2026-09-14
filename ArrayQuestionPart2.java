7a253ca5ec6724eb19f98895eb86860dd53d4a49
Pawan Kumar Yadav <pawanydv010@gmail.com>
2026-09-14 21:39:32 +0530

    Array Hashing Questions

--- ArrayQuestionPart2.java
+++ ArrayQuestionPart2.java
@@ -70,10 +70,42 @@ public class ArrayQuestionPart2 {
         }
         return maxFreqWaliKey;
     }
+
+    static int[] getHighestLowestFreqElement(int arr[]){
+        HashMap<Integer,Integer> freq = new HashMap<>();
+        for(int num: arr){
+            freq.put(num, freq.getOrDefault(num,0) + 1);
+        }
+        int highestFreq = Integer.MIN_VALUE;
+        int highestNum = -1;
+        for(int key: freq.keySet()){
+            int currentKey = key;
+            int currentFreq = freq.get(key);
+            if(currentFreq > highestFreq){
+                highestFreq = currentFreq;
+                highestNum = currentKey;
+            }
+        }
+        int lowestFreq = Integer.MAX_VALUE;
+        int lowestNum = -1;
+        for(int key: freq.keySet()){
+            int currentKey = key;
+            int currentFreq = freq.get(key);
+            if(currentFreq < lowestFreq){
+                lowestFreq = currentFreq;
+                lowestNum = lowestNum;
+            }
+        }
+        int ans[] = {highestFreq , lowestFreq};
+        return ans;
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,2,3,3,3,4,4,5,5,5,5,5};
-        int ans = getMode(arr);
-        System.out.println(ans);
+        int ans[] = getHighestLowestFreqElement(arr);
+        System.out.println("Highest Freq wala num " + ans[0]);
+        System.out.println("Lowest Freq wala num " + ans[1]);
+//        int ans = getMode(arr);
+//        System.out.println(ans);
 //        printAlternate(arr);
 //        reverseArray(arr);
 //        shiftBy1(arr);
